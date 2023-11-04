34a6826835bb0e698151db937059ce1a29494ef1
Luan Colombo <94877887+luancolombo@users.noreply.github.com>
2023-11-04 20:14:54 +0000

    fix forgottens knowledge

--- data-otservbr-global/scripts/actions/quests/forgotten_knowledge/thorn_knight_lever.lua
+++ data-otservbr-global/scripts/actions/quests/forgotten_knowledge/thorn_knight_lever.lua
@@ -1,7 +1,9 @@
 local config = {
 	boss = {
 		name = "The Enraged Thorn Knight",
-		position = Position(32624, 32880, 14),
+		createFunction = function()
+			return Game.createMonster("Mounted Thorn Knight", Position(32624, 32880, 14), true, true)
+		end,
 	},
 	requiredLevel = 250,
 	playerPositions = {
@@ -11,6 +13,11 @@ local config = {
 		{ pos = Position(32657, 32880, 14), teleport = Position(32624, 32886, 14), effect = CONST_ME_TELEPORT },
 		{ pos = Position(32657, 32881, 14), teleport = Position(32624, 32886, 14), effect = CONST_ME_TELEPORT },
 	},
+	onUseExtra = function(player)
+		for d = 1, 6 do
+			Game.createMonster("possessed tree", Position(math.random(32619, 32629), math.random(32877, 32884), 14), true, true)
+		end
+	end,
 	specPos = {
 		from = Position(32613, 32869, 14),
 		to = Position(32636, 32892, 14),
