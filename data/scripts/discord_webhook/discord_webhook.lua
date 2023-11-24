14a98a7a1ae44cb7a51834eba36004777ca3311c
Luan Colombo <94877887+luancolombo@users.noreply.github.com>
2023-11-24 16:23:10 +0000

    fix firewalker boots

--- data/scripts/discord_webhook/discord_webhook.lua
+++ data/scripts/discord_webhook/discord_webhook.lua
@@ -3,10 +3,10 @@
 -- Leave empty if you wish to disable.
 
 announcementChannels = {
-	["serverAnnouncements"] = "", -- Used for an announcement channel on your discord
+	["serverAnnouncements"] = "https://discord.com/api/webhooks/1156246862710444163/gCXHExkakhenpp2R8qRHmC_oVFUVnEX1nOTOkKhPr0danJAi25ouhl4xrxRSaKQTiyxn", -- Used for an announcement channel on your discord
 	["raids"] = "", -- Used to isolate raids on your discord
-	["player-kills"] = "", -- Self-explaining
-	["bans"] = "", -- Used for anunciate bans
+	["player-kills"] = "https://discord.com/api/webhooks/1156247501389693029/USWn8fnEuKqoo5lJoQkBE-Q1bQ14pqThG6WVpMMyAoQbcOfNUNuq1KL5ihheDESZYIZ0", -- Self-explaining
+	["bans"] = "https://discord.com/api/webhooks/1156244855647912057/ix6RzIUUEKNNXFr0GWYXNAu2HVw0JZHahxhDAzCQapIoQ4rutQP2ozeT89IfnSTIRg8K", -- Used for anunciate bans
 }
 
 --[[
