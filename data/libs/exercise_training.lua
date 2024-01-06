3a772ed08861f516f536b7660d6abc399f402592
Luan Colombo <94877887+luancolombo@users.noreply.github.com>
2024-01-06 19:52:15 +0000

    update exercise weapons

--- data/libs/exercise_training.lua
+++ data/libs/exercise_training.lua
@@ -114,9 +114,12 @@ function ExerciseEvent(playerId, tilePosition, weaponId, dummyId)
 
 	if weapon:getAttribute(ITEM_ATTRIBUTE_CHARGES) <= 0 then
 		weapon:remove(1)
-		player:sendTextMessage(MESSAGE_EVENT_ADVANCE, "Your training weapon has disappeared.")
-		LeaveTraining(playerId)
-		return false
+		local weapon = player:getItemById(weaponId, true)
+		if not weapon or (not weapon:isItem() or not weapon:hasAttribute(ITEM_ATTRIBUTE_CHARGES)) then
+			player:sendTextMessage(MESSAGE_EVENT_ADVANCE, "Your training weapon has disappeared.")
+			LeaveTraining(playerId)
+			return false
+		end
 	end
 
 	local vocation = player:getVocation()
