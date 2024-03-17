a056dc839aa4d1ab44501f0bbf9c42b988fc2605
Luan Colombo <94877887+luancolombo@users.noreply.github.com>
2024-03-17 19:35:02 +0000

    Revert "feat: refresh market averages on a clock (#1995)"
    This reverts commit 029fee6e32f0210f2e8ef60816cdc967415ece84.

--- src/server/network/protocol/protocolgame.cpp
+++ src/server/network/protocol/protocolgame.cpp
@@ -7810,21 +7810,18 @@ void ProtocolGame::sendItemsPrice() {
 	NetworkMessage msg;
 	msg.addByte(0xCD);
 
-	auto countBuffer = msg.getBufferPosition();
-	uint16_t count = 0;
-	msg.skipBytes(2);
-	for (const auto &[itemId, tierAndPriceMap] : g_game().getItemsPrice()) {
-		for (const auto &[tier, price] : tierAndPriceMap) {
-			msg.add<uint16_t>(itemId);
-			if (Item::items[itemId].upgradeClassification > 0) {
-				msg.addByte(tier);
+	msg.add<uint16_t>(g_game().getItemsPriceCount());
+	if (g_game().getItemsPriceCount() > 0) {
+		for (const auto &[itemId, tierAndPriceMap] : g_game().getItemsPrice()) {
+			for (const auto &[tier, price] : tierAndPriceMap) {
+				msg.add<uint16_t>(itemId);
+				if (Item::items[itemId].upgradeClassification > 0) {
+					msg.addByte(tier);
+				}
+				msg.add<uint64_t>(price);
 			}
-			msg.add<uint64_t>(price);
-			count++;
 		}
 	}
-	msg.setBufferPosition(countBuffer);
-	msg.add<uint16_t>(count);
 
 	writeToOutputBuffer(msg);
 }
